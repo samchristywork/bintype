71a22f45214ecc0b4494e4b62cd4d57416f0f7ae
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-09-02 22:11:50 -0500

    feat: Add basic program
    This simple program defines the data structure we are going to use for
    heuristics. The heuristics will be collected in a tsv file for now.

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+class heuristic {
+public:
+  std::string language;
+  std::string directive;
+  std::string data;
+};
+
+int main() {
+  // Collect heuristics
+  std::vector<heuristic> heuristics;
+}
