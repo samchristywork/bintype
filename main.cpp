4b5fef8a576fe188aea0ce2b7d921bb3b0f4d5b7
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-09-02 22:13:29 -0500

    feat: Add simple function for parsing tsv files
    This program assumes a three field per line tsv file called
    "heuristics".

--- main.cpp
+++ main.cpp
@@ -11,7 +11,33 @@ public:
   std::string data;
 };
 
+void parse_tsv(std::string filename, std::vector<heuristic> &heuristics) {
+  std::ifstream file(filename);
+
+  if (!file.is_open()) {
+    std::cout << "Unable to open file: " << filename << std::endl;
+    return;
+  }
+
+  std::string line;
+  while (std::getline(file, line)) {
+    std::istringstream iss(line);
+    std::string language;
+    std::string directive;
+    std::string data;
+    std::getline(iss, language, '\t');
+    std::getline(iss, directive, '\t');
+    std::getline(iss, data, '\t');
+    heuristic h;
+    h.language = language;
+    h.directive = directive;
+    h.data = data;
+    heuristics.push_back(h);
+  }
+}
+
 int main() {
   // Collect heuristics
   std::vector<heuristic> heuristics;
+  parse_tsv("heuristics", heuristics);
 }
